d33efec0b8e19726db924ca626849e64b3875643
Karina Barinova <kbarinova11@gmail.com>
2023-02-21 15:41:01 +0000

    Implement parseInt pipe

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -4,6 +4,7 @@ import {CreateCoffeeDto} from './dto/create-coffee.dto/create-coffee.dto';
 import {UpdateCoffeeDto} from './dto/update-coffee.dto/update-coffee.dto';
 import {PaginationQueryDto} from 'src/common/dto/pagination-query.dto/pagination-query.dto';
 import {Public} from 'src/common/decorators/public.decorator';
+import {ParseIntPipe} from 'src/common/pipes/parse-int/parse-int.pipe';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -16,7 +17,8 @@ export class CoffeesController {
     }
 
     @Get(':id')
-    findOne(@Param('id') id: number) {
+    findOne(@Param('id', ParseIntPipe) id: number) {
+        console.log(id)
         return this.coffeesService.findOne('' + id)
     }
 
