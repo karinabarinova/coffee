925f46cd55f2bf9152704084d582f9e26b914856
Karina Barinova <kbarinova11@gmail.com>
2023-02-21 11:23:35 +0000

    Example for factory providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,4 +1,4 @@
-import {Module} from '@nestjs/common';
+import {Injectable, Module} from '@nestjs/common';
 import {CoffeesController} from './coffees.controller';
 import {CoffeesService} from './coffees.service';
 import {TypeOrmModule} from '@nestjs/typeorm';
@@ -10,16 +10,25 @@ class ConfigService {}
 class DevelopmentConfigService {}
 class ProductionConfigService {}
 
+@Injectable()
+export class CoffeeBrandsFactory {
+    create() {
+        return ['buddy brew', 'nescafe']
+    }
+}
+
 @Module({
     imports: [TypeOrmModule.forFeature(
         [Coffee, Flavor, Event]
     )],
     exports: [CoffeesService],
     controllers: [CoffeesController],
-    providers: [CoffeesService, {
-        provide: COFFEE_BRANDS,
-        useValue: ['buddy brew', 'nescafe']
-    },
+    providers: [CoffeesService,
+        CoffeeBrandsFactory,
+        {
+            provide: COFFEE_BRANDS,
+            useFactory: (brandsFactory: CoffeeBrandsFactory) => brandsFactory.create(), inject: [CoffeeBrandsFactory]
+        },
         {
             provide: ConfigService,
             useClass: process.env.NODE_ENV === 'development' ? DevelopmentConfigService : ProductionConfigService
