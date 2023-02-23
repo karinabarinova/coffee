0c6cda068ca24f9fcdeb1a4fc9dc7fcb0ed34e0c
Karina Barinova <kbarinova11@gmail.com>
2023-02-23 14:31:27 +0000

    Add a tag to group resources

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -6,8 +6,9 @@ import {PaginationQueryDto} from 'src/common/dto/pagination-query.dto/pagination
 import {Public} from 'src/common/decorators/public.decorator';
 import {ParseIntPipe} from 'src/common/pipes/parse-int/parse-int.pipe';
 import {Protocol} from 'src/common/decorators/protocol.decorator';
-import {ApiForbiddenResponse} from '@nestjs/swagger';
+import {ApiForbiddenResponse, ApiTags} from '@nestjs/swagger';
 
+@ApiTags('coffees')
 @Controller('coffees')
 export class CoffeesController {
     constructor (private readonly coffeesService: CoffeesService) {}
