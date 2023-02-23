fa5ac02817011a332d1f009727b285f83f94a0e9
Karina Barinova <kbarinova11@gmail.com>
2023-02-23 14:24:36 +0000

    Set up swagger

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import {ValidationPipe} from '@nestjs/common';
 import {HttpExceptionFilter} from './common/filters/http-exception/http-exception.filter';
 import {WrapResponseInterceptor} from './common/interceptors/wrap-response/wrap-response.interceptor';
 import {TimeoutInterceptor} from './common/interceptors/timeout/timeout.interceptor';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true, //removes properties not listed in corresponding DTOs
     transform: true, //transforms payload to its corresponding type, make primitive types conversion
@@ -17,6 +19,15 @@ async function bootstrap() {
   }))
   app.useGlobalFilters(new HttpExceptionFilter())
   app.useGlobalInterceptors(new WrapResponseInterceptor(), new TimeoutInterceptor())
+
+  const options = new DocumentBuilder()
+    .setTitle('Iluvcoffee')
+    .setDescription('Coffee app')
+    .setVersion('1.0')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('api', app, document)
   await app.listen(3000);
 }
 bootstrap();
