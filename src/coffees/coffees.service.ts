7af3faed9c4d2c866b43b2bab57e33c19c0cd56b
Karina Barinova <kbarinova11@gmail.com>
2023-02-23 15:17:20 +0000

    Add unit tests for Coffee.findOne()

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -6,9 +6,8 @@ import {CreateCoffeeDto} from './dto/create-coffee.dto/create-coffee.dto';
 import {UpdateCoffeeDto} from './dto/update-coffee.dto/update-coffee.dto';
 import {Flavor} from './entities/flavor.entity';
 import {PaginationQueryDto} from 'src/common/dto/pagination-query.dto/pagination-query.dto';
-import {Event} from 'src/events/entities/event.entity/event.entity';
-import {COFFEE_BRANDS} from './coffees.constants';
-import {ConfigService, ConfigType} from '@nestjs/config';
+import {Event} from './../events/entities/event.entity/event.entity';
+import {ConfigType} from '@nestjs/config';
 import coffeesConfig from './config/coffees.config';
 
 @Injectable()
@@ -22,7 +21,6 @@ export class CoffeesService {
         @Inject(coffeesConfig.KEY)
         private readonly coffeesConfiguration: ConfigType<typeof coffeesConfig>,
     ) {
-        console.log(coffeesConfiguration)
     }
 
     findAll(paginationQuery: PaginationQueryDto) {
